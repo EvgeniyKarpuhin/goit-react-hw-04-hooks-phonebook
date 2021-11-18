5f0b51d364f26cab6196e64b95b5bde317eabded
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-11-18 23:42:58 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import shortid from "shortid";
+import ContactForm from "./components/ContactForm/ContactForm ";
+import Filter from "./components/Filter/Filter";
+import ContactList from "./components/ContactList/ContactList";
 
-function App() {
+export default function App() {
+  
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState();
+  const [name, setName] = useState();
+  const [number, setNumber] = useState();
+
+  function handleChange(evt) {
+
+    switch (evt.target.name) {
+      case 'name':
+        setName(evt.target.value);
+        break;
+      
+      case 'tel':
+        setNumber(evt.target.value);
+        break;
+    
+      default:
+        break;
+    }
+  };
+
+  function handleAddContact () {
+    if (
+      contacts.find((contact) => {
+        return contact.name === name;
+      })
+    ) {
+      alert(`${name} is already in contacts`);
+    } else {
+      const contactsArr = contacts;
+      const newContact = {
+        key: shortid.generate(),
+        name: name,
+        number: number,
+      };
+
+      setContacts([...contactsArr, newContact]);
+
+      // LocalStorage-------
+
+      if (localStorage.getItem("contacts") === null) {
+        localStorage.setItem(
+          "contacts",
+          JSON.stringify([...contactsArr, newContact])
+        );
+      } else {
+        const temperaryArr = JSON.parse(localStorage.getItem("contacts"));
+        temperaryArr.push(newContact);
+        localStorage.setItem("contacts", JSON.stringify(temperaryArr));
+      };
+      // -------------------
+    };
+  };
+
+  function filterContacts (evt) {
+    return setFilter(evt.target.value);
+  };
+
+  function deleteContact (evt) {
+    const contactsArr = contacts;
+    const id = evt.target.id;
+    const elementForRemove = contacts.find(
+      (item) => item.key === id
+    );
+    const index = contactsArr.indexOf(elementForRemove);
+    contactsArr.splice(index, 1);
+
+    // LocalStorage-------
+    localStorage.removeItem("contacts");
+    localStorage.setItem("contacts", JSON.stringify(contactsArr));
+    // --------------------
+
+    setContacts([...contactsArr]);
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container">
+      <h1>Phonebook</h1>
+      <ContactForm
+        handleChange={handleChange}
+        handleAddContact={handleAddContact}
+      />
 
-export default App;
+      <h2>Contacts</h2>
+      <Filter
+        filterContacts={filterContacts}
+      />
+
+      <ContactList
+        filter={filter}
+        contacts={contacts}
+        key={contacts.key}
+        deleteContact={deleteContact}
+      />
+    </div>
+  
+  );
+};
